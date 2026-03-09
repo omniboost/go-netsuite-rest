c894eb17fa233a144cb2ae23c9a11ee6f95e6f1c
Leon Bogaert <leon@omniboost.io>
2026-03-09 13:12:53 +0100

    fix: content-type checking

--- client.go
+++ client.go
@@ -538,7 +538,7 @@ func (d *ErrorDetail) Error() string {
 func checkContentType(response *http.Response) error {
 	header := response.Header.Get("Content-Type")
 	contentType := strings.Split(header, ";")[0]
-	if contentType != "application/vnd.oracle.resource+json" {
+	if contentType != "application/json" {
 		return fmt.Errorf("Expected Content-Type \"%s\", got \"%s\"", mediaType, contentType)
 	}
 
